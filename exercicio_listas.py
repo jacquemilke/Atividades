c2b0b9dc18ef4c1641a4252afa7da3f63e20f2ed
jacquemike <jacquelinee63035896@alunos.sc.senac.br>
2026-05-06 20:59:24 -0300

    exercicio_lista2

--- exercicio_listas.py
+++ exercicio_listas.py
@@ -1,7 +1,19 @@
 produtos = ["Fone de ouvido", "Celular", "Notebook", "Mouse", "Projetor"]
-quantidade = len(produtos)
+precos = [40.00, 80.00, 15.00, 20.00, 20.00]
 
-print(produtos)
-print(produtos[0])
-print(produtos[-1])
-print(quantidade)
+print(f"O produto {produtos[0]} custa R$ {precos[0]}.")
+produtos.remove(produtos[-1])
+precos.remove(precos[-1])
+
+total_precos = sum(precos)
+print(f"O total deu R${total_precos}.")
+
+if total_precos < 100:
+    exit()
+
+else:
+    desconto = 0.95
+    total_geral = total_precos * desconto
+    descontoPercentual = (1 - desconto) * 100
+    descontoPercentual = round(descontoPercentual,0)
+    print(f"Sua compra deu R${total_precos}. Você ganhou {descontoPercentual}% de desconto. O total agora é R$ {total_geral}")    
