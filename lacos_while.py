37451f922c0528617d47896f4903be798f31e61f
jacquemike <jacquelinee63035896@alunos.sc.senac.br>
2026-05-11 21:53:09 -0300

    aula

--- lacos_while.py
+++ lacos_while.py
@@ -12,3 +12,19 @@ while True:
     if resposta == "sair":
         break
 
+while True:
+    mensagem = f"""
+    Menu:
+    1 - Ver sutuação 1
+    2 - Ver situação 2
+    3 - Sair
+    """
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        elif opcao == "2":
+            elif opcao == "3":
+                print("Sair do sistema...")
+                break
+    else:
+        print("Opção inválida. Tente novamente.")
