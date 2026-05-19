2eefd8884e0e833b0b8251830f6252c3e0a9ccb8
jacquemilke <jacquemilke@gmail.com>
2026-05-18 21:48:13 -0300

    18-05-26

---
+++ resumo.py
@@ -0,0 +1,53 @@
+"""
+Blocos fundamentais de um algorítmo:
+entrada -> processamento -> saída
+
+--------------------------
+
+Operadores aritméticos:
+utilizados para operações matemáticas
++ 
+-
+*
+/
+% (módulo) - divisão inteira
+
+--------------------------
+
+Variáveis: espaço na memória
+volátil (RAM) - muito rápida, porém temporária
+não volátil (DH/SSD) - mais lenta, porém permanente
+
+sintaxe
+nome = Ana
+idade = 30
+
+inteiro (int) - números sem parte fracionária - 1, 2, 3
+real (float/double) - números com casas decimais - 0.25, 1.50, 2.7532
+string - texto ou cadeia de caracteres - "Olá, mundo!"
+Char - um caractere único - "A", "B", "C"...
+Boolean - True/False - Verdadeiro ou falso
+Data/Time - 20/03/2025, 15:30:45 - Datas e horas formatadas
+
+--------------------------
+
+Função input()
+
+sintaxe
+nome = input("Digite seu nome: ")
+numero = int(input("Digite um numero: "))
+
+--------------------------
+
+Operadores lógicos e relacionais
+!= - diferente de
+&& - And
+|| - Or
+
+--------------------------
+
+if/elif/else
+
+
+
+"""
---
+++ revisao2.py
@@ -0,0 +1,11 @@
+#Declare 3 variáveis: uma é o nome do produto, outra para armazenar o preço do produto e a outra a quantidade comprada desse produto.
+#calcule o total
+#exiba uma mensagem formatada com o nome e o total
+
+nome_produto = "Camisa"
+preco_produto = 22.40
+quantidade_comprada = 2
+
+total = preco_produto * quantidade_comprada
+
+print(f"O produto comprado foi {nome_produto} e a quantidade foi {quantidade_comprada}. Totalizando R$ {total}")
