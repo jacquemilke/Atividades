144edaa257ec3a6a85a7bb23f5b9141f1ea533b5
jacquemike <jacquelinee63035896@alunos.sc.senac.br>
2026-05-27 20:14:03 -0300

    27_05_26

---
+++ Desafio_Auto_Pecas_Oficina.py
@@ -0,0 +1,46 @@
+produtos = ["Pneu", "Bateria", "Óleo", "Palheta"]
+precos_produtos = [400.00, 150.00, 50.00, 30.00]
+
+servicos = ["Alinhamento", "Revisão", "Lavagem"]
+precos_servicos = [500.00, 150.00, 50.00]
+
+escolha_tipo = input("Você deseja ver nossos Produtos ou Serviços? ")
+
+if escolha_tipo == "Produto":
+    print("\nProdutos disponíveis:")
+    for indice, produto in enumerate(produtos):
+        preco_produto = precos_produtos [indice]
+        print(f"{produto} custa - R$ {preco_produto}")
+
+elif escolha_tipo == "Serviço":
+    for indice, servico in enumerate(servicos):
+        preco_servico = precos_servicos [indice]
+        print(f"{servico} custa - R$ {preco_servico}")
+
+else:
+    print("Opção inválida")
+    exit
+
+codigo = int(input("Digite o número do item que deseja comprar: "))
+
+if escolha_tipo == "Produto":
+    nome_escolhido = produtos[codigo]
+    preco_escolhido = precos_produtos[codigo]
+
+elif escolha_tipo == "Serviço":
+    nome_escolhido = servicos[codigo]
+    preco_escolhido = precos_servicos[codigo]
+
+if preco_escolhido >= 300:
+    preco_escolhido = round(preco_escolhido * 0.90, 2)
+
+mensagem = f"""
+    +--------------------------------------------+
+    |                 CUPOM FISCAL               |
+    +--------------------------------------------+
+    Produto: {nome_escolhido}
+    Valor: R$ {preco_escolhido}
+    
+    """
+
+print(mensagem)
--- atividade_while3.py
+++ atividade_while3.py
@@ -1,50 +1,66 @@
-#loja automotiva. Vendas de produtos. Serviços. usário entra com pedido de serviço ou produto. Mostrar a lista de produto ou serviço. Listas com nomes dos produtos e outra lista com nomes dos produtos. 
-
-produtos = ["Cera", "Pneu", "Óleo"]
-servicos = ["Troca de óleo", "Troca filtro", "Trocar bateria"]
-valor_produtos = [20.50, 250.00, 100.40]
-valor_servicos = [50.00, 85.60, 185.90]
-cod_produto = [2]
-
-
-print(f"""
-      +--------------------------------------------+
-      |         BEM - VINDO A AUTO CENTER          |
-      +--------------------------------------------+
-       
-      Digite 1 para "Produtos"
-      Digite 2 para "Serviços"
-          
-      +--------------------------------------------+
-    """)
-
-usuario = input("")
-
-if usuario == "1":
-    for indice, produto in enumerate(produtos):
-        valor_produto = valor_produtos [indice]
-        print(f"{produto} custa R$ {valor_produto}")
-
-
-elif usuario == "2":
-    for indice, servico in enumerate(servicos):
-        valor_servico = valor_servicos [indice]
-        print(f"{servico} custa R$ {valor_servico}")
-
+# Listas de produtos e preços
+produtos = ["Pneu", "Bateria", "Óleo", "Palheta"]
+precos_produtos = [400.00, 350.00, 50.00, 30.00]
+ 
+# Listas de serviços e preços
+servicos = ["Alinhamento", "Revisão", "Lavagem"]
+precos_servicos = [100.00, 150.00, 50.00]
+ 
+# Pergunta ao cliente
+print("Você deseja ver nossos Produtos ou Serviços?")
+escolha = input()
+ 
+# Exibe produtos
+if escolha == "Produto":
+    print("Produtos disponíveis:")
+    i = 0
+    while i < len(produtos):
+        print(str(i) + " - " + produtos[i] + " - R$ " + str(precos_produtos[i]))
+        i = i + 1
+    categoria = "produto"
+ 
+# Exibe serviços
+elif escolha == "Serviço":
+    print("Serviços disponíveis:")
+    i = 0
+    while i < len(servicos):
+        print(str(i) + " - " + servicos[i] + " - R$ " + str(precos_servicos[i]))
+        i = i + 1
+    categoria = "serviço"
+ 
+# Opção inválida
 else:
-    print("Valor incorreto. Digite novamente.")
-
-cod_produto = int(input("Digite o código escolhido: "))
-
-
-
-print(f"""
-      +--------------------------------------------+
-      |                CUPOM FISCAL                |
-      +--------------------------------------------+
-      Item comprado: R$
-      Desconto: R$
-      O valor total: R$:
-          
-      +--------------------------------------------+
-    """)
+    print("Opção inválida.")
+    exit()
+ 
+# Pede para o cliente escolher um item
+print("Digite o número do item que deseja comprar:")
+indice_item = int(input())
+ 
+# Verifica qual item foi escolhido
+if categoria == "produto":
+    if indice_item >= 0 and indice_item < len(produtos):
+        nome_item = produtos[indice_item]
+        preco_original = precos_produtos[indice_item]
+    else:
+        print("Número inválido.")
+        exit()
+else:
+    if indice_item >= 0 and indice_item < len(servicos):
+        nome_item = servicos[indice_item]
+        preco_original = precos_servicos[indice_item]
+    else:
+        print("Número inválido.")
+        exit()
+ 
+# Aplica desconto se for maior que 300
+if preco_original > 300:
+    preco_final = preco_original - (preco_original * 0.10)
+else:
+    preco_final = preco_original
+ 
+# Mostra o cupom fiscal
+print("--------- CUPOM FISCAL ---------")
+print("Item comprado: " + nome_item)
+print("Valor a pagar: R$ " + str(preco_final))
+print("--------------------------------")
